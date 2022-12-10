5a5f41c0866eb4e552d3821f0a30a48bae6cc97f
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-10 09:32:06 +0100

    chore: making some changes

--- src/component/BtnRender.tsx
+++ src/component/BtnRender.tsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export const BtnRender = ({ hotelid }: { hotelid: string }) => {
   return (
     <Fragment>
-      <Link to={`/detail/${hotelid}`} id="details-btn">
-        <button>details</button>
+      <Link to={`/detail/${hotelid}`}>
+        <button id="details-btn">details</button>
       </Link>
-      <Link to={`/edit/${hotelid}`} id="edit-btn">
-        <button>Edit</button>
+      <Link to={`/edit/${hotelid}`}>
+        <button id="edit-btn">Edit</button>
       </Link>
     </Fragment>
   );
