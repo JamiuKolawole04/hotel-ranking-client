f0c5418f8873e13b5d0b3b459daa5757b1166204
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-11 11:16:09 +0100

    chore: adding server url

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080" as string, // THE API (backend) URL
+  baseURL: "https://api-hotel-ranking.up.railway.app" as string, // THE API (backend) URL
 });
 
 export default instance;
